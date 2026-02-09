53bfac5d25760703695a426a2c3c63f1b09e8522
Nicolas <nkpv327@gmail.com>
2026-02-09 11:25:59 +0000

    Starting mini pipeline to merge datasets for app

---
+++ pipeline.py
@@ -0,0 +1,6 @@
+"""
+This contains a small and simple data pipeline that ingests the clean data, standardizes postcode columns and
+joins the three given datasets. The output is a csv that contains all micro, small and medium charities in 
+Tower Hamlets.
+"""
+
