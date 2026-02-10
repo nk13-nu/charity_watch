f5dab0e09244bcfaa54249f6a0264f83dcb5d554
Nicolas <nkpv327@gmail.com>
2026-02-10 14:57:32 +0000

    Exploring the dataset and displying information on the app: number of charities per size

--- charity_watcher/src/App.jsx
+++ charity_watcher/src/App.jsx
@@ -2,9 +2,22 @@ import charities from '../data/charities_with_deprivation.json'
 
 function App(){
 
-  const micro = charities.filter(c => c['Size Band'] === 'Micro').length;
-  const small;
-  const medium;
+  const micro = charities.filter(c => c['Charity Size Band'] === 'Micro').length;
+  const small = charities.filter(c => c['Charity Size Band'] === "Small").length;
+  const medium = charities.filter(c => c['Charity Size Band'] === "Medium").length;
+
+  return(
+    <div style={{ padding: '40px' }}>
+      <h1>Charity Watcher</h1>
+      <p>There are {charities.length} charities on record</p>
+
+      <h2>Tower Hamlets Charities By size:</h2>
+      <p>Micro: {micro}</p>
+      <p>Small: {small}</p>
+      <p>Medium: {medium}</p>
+    </div>
+  )
+
 }
 
 export default App
