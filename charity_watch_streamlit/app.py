c041d974df89d08a29bc5c6f7d4c56c85c909bd0
Nicolas <nkpv327@gmail.com>
2026-02-12 13:59:05 +0000

    starting streamlit app based on wireframe

--- charity_watch_streamlit/app.py
+++ charity_watch_streamlit/app.py
@@ -1,6 +1,14 @@
-import streamlit as st
-import pandas as pd
-import json
+#Imports
+import streamlit as st #streamlit for the app
+import pandas as pd #pandas for data manipulation
+import json #json to work with json files
 import folium
 from streamlit_folium import st_folium
+import plotly.express as px
+
+#Setting global page configurations
+st.set_page_config(page_title="Charity Watch", layout='Wide', initial_sidebar_state='expanded')
+
+#Importing dataset
+df = pd.read_json("data/charities_with_deprivation.json")
 
---
+++ lsoa_imd.py
@@ -0,0 +1,16 @@
+import json
+import pandas as pd
+
+df = pd.read_csv('data/clean_data/deprivation_th.csv')
+
+lsoa_to_imd_mapping = {}
+for _, r in df.iterrows():
+    lsoa_to_imd_mapping[r["LSOA code (2021)"]] = {
+        "name": r["LSOA name (2021)"],
+        "imdScore": round(r["Index of Multiple Deprivation (IMD) Score"], 2),
+        "imdDecile": int(r["Index of Multiple Deprivation (IMD) Decile (where 1 is most deprived 10% of LSOAs)"]),
+        "population": int(r["Total population: mid 2022"]),
+    }
+
+with open("lsoa_to_imd_mapping.json", "w") as f:
+    json.dump(lsoa_to_imd_mapping, f, indent=2)
