245f5f79685ec969c83a6ecf2180a1f8acf5a5e6
Nicolas <nkpv327@gmail.com>
2026-02-14 11:55:03 +0000

    starting layout

--- charity_watch_streamlit/app.py
+++ charity_watch_streamlit/app.py
@@ -5,18 +5,19 @@ import json #json to work with json files
 import folium
 from streamlit_folium import st_folium
 import plotly.express as px
+import pydeck as pdk
 
 #importing services
-from charity_watch_streamlit.services.load_data import load_data
 
 
 
 def main():
-    st.set_page_config(page_title="Charity Watch", layout='wide', initial_sidebar_state='expanded')
+    st.set_page_config(page_title="Charity Watch", layout='wide', initial_sidebar_state='collapsed')
+    background_image = 'charity_watch_streamlit/resources/tower_hamlets_image.jpg'
 
+    st.title("Charity Watch")
 
-
-
+    st.text_input("X", placeholder="X")
 
 if __name__ == "__main__":
     main()
