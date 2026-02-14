190a7e82e1e86eff595b9985e369d1f2381a219a
Nicolas <nkpv327@gmail.com>
2026-02-14 16:13:45 +0000

    added comments

--- charity_watch_streamlit/app.py
+++ charity_watch_streamlit/app.py
@@ -13,7 +13,10 @@ from pathlib import Path
 from charity_watch_streamlit.services.load_data import (load_charities as _load_charities, 
                                                         load_geojson as _load_geojson, 
                                                         load_imd as _load_imd)
+from charity_watch_streamlit.services.statistics_and_helpers import identify_comissioning_gaps
 
+
+#Initial page configuration
 st.set_page_config(page_title="Charity Watch", layout='wide', initial_sidebar_state='collapsed')
 
 
@@ -21,6 +24,7 @@ st.set_page_config(page_title="Charity Watch", layout='wide', initial_sidebar_st
 ################ LOADING DATA #################
 ###############################################
 
+#we cache data for faster retrieval and create local load functions for the three datasets
 @st.cache_data
 def load_charities(path: str):
     return _load_charities(path)
@@ -33,9 +37,15 @@ def load_geojson(path: str):
 def load_imd(path: str):
     return _load_imd(path)
 
+#now we load data 
 df = load_charities("data/charities_with_deprivation.json")
 lsoa_geo = load_geojson("data/lsoa_clean.geojson")
 lsoa_imd = load_imd("data/lsoa_to_imd_mapping.json")
+
+@st.cache_data
+def get_gap_codes():
+    """Return the set of LSOA codes that are commissioning gaps withing Tower hamlets"""
+    return {g["code"] for g in identify_comissioning_gaps()}
 
 ###############################################
 ###############################################
