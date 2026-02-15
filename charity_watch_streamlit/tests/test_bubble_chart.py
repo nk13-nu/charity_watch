a15231d9cca2f676a2db959cb198c0d881bad48b
Nicolas <nkpv327@gmail.com>
2026-02-15 12:44:18 +0000

    added test_bubble_chart.py to test bubble chart method

---
+++ charity_watch_streamlit/tests/test_bubble_chart.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import plotly.express as px
+import pytest
+
+def test_build_bubble_chart_returns_px_scatter():
+    pass
+
+def test_build_bubble_chart_wrong_argument_type_raises_type_error():
+    pass
