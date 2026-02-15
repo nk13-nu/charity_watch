e4d339b219ce6f4bf1e4746608f5f078ffc75e2e
Nicolas <nkpv327@gmail.com>
2026-02-15 00:54:30 +0000

    all statistics ready, after map is clicked we now get stats for selected lsoa. Currently as st messages, later implement custom styling

--- charity_watch_streamlit/app.py
+++ charity_watch_streamlit/app.py
@@ -14,7 +14,7 @@ from charity_watch_streamlit.style.style import app_style_design, APP_COLOUR_PAL
 from charity_watch_streamlit.services.load_data import (load_charities as _load_charities, 
                                                         load_lsoa_gdf as _load_lsoa_gdf, 
                                                         load_imd as _load_imd)
-from charity_watch_streamlit.services.helpers import identify_comissioning_gaps, deprivation_colour, retrieve_lsoa_specific_data_from_click
+from charity_watch_streamlit.services.helpers import identify_comissioning_gaps, deprivation_colour, retrieve_lsoa_specific_data_from_click, income_formatting
 from charity_watch_streamlit.services.map import build_map
 from charity_watch_streamlit.services.bubble_chart import build_bubble_chart
 
@@ -101,3 +101,46 @@ with info_columns:
         st.text(lsoa_name)
         st.text(score)
         st.text(population)
+
+
+#if an lsoa is clicked on the map
+if clicked_lsoa:
+    #we retrieve all imd information
+    imd_info = lsoa_imd.get(clicked_lsoa, {})
+    #get the lsoa name
+    lsoa_name = imd_info.get("name", clicked_lsoa)
+    #and all data about charities within the lsoa
+    charities_here = df[df["lsoaCode"] == clicked_lsoa]
+
+    #display text signaling which lsoa is selected
+    st.text(f'Showing: {lsoa_name}')
+
+    #set 4 columns
+    c1, c2, c3, c4 = st.columns(4)
+    #on column 1 we place number of charities in lsoa
+    with c1:
+        st.text(f"Number of Charities: {len(charities_here)}")
+    #column 2 takes the total income of the lsoa's charities
+    with c2:
+        st.text(f"Total Last Recorded Income: {income_formatting(charities_here["income"].sum())}")
+    #column3 we pass the deprivation score of the lsoa
+    with c3:
+        st.text(f"Deprivation Score: {imd_info.get("imdScore", "—")}")
+    #column 4 for the population
+    with c4:
+        st.text(f"Population: {imd_info.get("population", 0):,}")
+else:
+    #if there is no selected lsoa on the map we go back to defaults
+    c1, c2, c3, c4 = st.columns(4)
+    #ww first display the total number of (small, medium, micro) charities in the dataset/tower hamlets
+    with c1:
+        st.text(f"Number of Charities: {len(df)}")
+    #calculate avergae last recorded income across borough and display it
+    with c2:
+        st.text(f"Average Last Recorded Income: {income_formatting(df["income"].mean())}")
+    #calculate average deprivation score 
+    with c3:
+        st.text(f"Average Deprivation Score {df["imdScore"].mean()}")
+    #and display the lsoa charity coverage across tower hamlets
+    with c4:
+        st.text(f"LSOA Charity Coverage {df["lsoaCode"].nunique()}/{len(df)}")
--- charity_watch_streamlit/services/bubble_chart.py
+++ charity_watch_streamlit/services/bubble_chart.py
@@ -1,4 +1,5 @@
 import plotly.express as px
+import pandas as pd
 
 def build_bubble_chart():
     pass
