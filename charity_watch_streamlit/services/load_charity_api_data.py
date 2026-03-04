f677c8d52d9192de3da4cda05670df2fa07baadc
Nicolas <nkpv327@gmail.com>
2026-03-04 23:05:57 +0000

    finalised method to retrieve financial data

--- charity_watch_streamlit/services/load_charity_api_data.py
+++ charity_watch_streamlit/services/load_charity_api_data.py
@@ -6,3 +6,22 @@ load_dotenv()
 
 
 API_KEY = os.getenv("CCEW_API_KEY")
+
+#now we define a function to retreive financial charities financial history from the charity commission's api
+def get_charity_financial_history(charity_id:int) -> dict:
+    """
+    Gets 5 year charity financial history from the Charity Commission API using the charity id
+    """
+    #set the targeted api endpoint
+    url = f"https://api.charitycommission.gov.uk/register/api/charityfinancialhistory/{charity_id}/0"
+    #required headers
+    headers = {
+        "Ocp-Apim-Subscription-Key": API_KEY,
+        "Accept": "application/json"
+    }
+    #save the response
+    response = requests.get(url, headers=headers)
+    #raise for status to see if there are any errors
+    response.raise_for_status()
+    #and we return a json to be able to access it as a dictionary
+    return response.json()
