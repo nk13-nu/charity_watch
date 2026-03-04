2f78f70d7aad9314709e4931370662236f4b8394
Nicolas <nkpv327@gmail.com>
2026-03-04 14:07:23 +0000

    established function and added docstring

--- charity_watch_streamlit/services/api_line_chart.py
+++ charity_watch_streamlit/services/api_line_chart.py
@@ -1,1 +1,7 @@
+import pandas as pd
 import plotly.express as px
+
+def build_charity_income_line_chart(df:pd.DataFrame):
+    """
+    This function plots the data received from the Charity Commission API in a line chart for a selected charity.
+    """
