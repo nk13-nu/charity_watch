db0a9ea8c01c901a9259d186970cd09275254d68
nk13-nu <kuriperezvillaman.n@northeastern.edu>
2026-03-16 07:13:01 +0000

    Update test_load_data.py

--- charity_watch_streamlit/tests/test_load_data.py
+++ charity_watch_streamlit/tests/test_load_data.py
@@ -1,5 +1,5 @@
 import pytest
-from charity_watch_streamlit.services.load_data import load_charities, load_geojson, load_imd
+from charity_watch_streamlit.services.load_data import load_charities, load_imd
 
 def test_load_charities_invalid_path_suffix_error_expected():
     dummy_path = 'somethin/data.jsonnn'
@@ -11,15 +11,15 @@ def test_load_charities_path_does_not_exist_raised_error_expected():
     with pytest.raises(ValueError):
         load_charities(dummy_path)
 
-def test_load_geojson_invalid_file_type_error_expected():
-    dummy_path = 'somethin/data.json'
-    with pytest.raises(ValueError):
-        load_geojson(dummy_path)
+#def test_load_geojson_invalid_file_type_error_expected():
+#    dummy_path = 'somethin/data.json'
+#    with pytest.raises(ValueError):
+#        load_geojson(dummy_path)
 
-def test_load_geojson_path_does_not_exist_raised_error_expected():
-    dummy_path = 'somethin/data.jsonnn'
-    with pytest.raises(ValueError):
-        load_geojson(dummy_path)
+#def test_load_geojson_path_does_not_exist_raised_error_expected():
+#    dummy_path = 'somethin/data.jsonnn'
+#    with pytest.raises(ValueError):
+#        load_geojson(dummy_path)
 
 def test_load_imd_invalid_file_type_error_expected():
     dummy_path = 'somethin/data.geojson'
